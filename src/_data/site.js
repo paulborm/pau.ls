47c13f6f9a5a01f40889c5574b7fffe99137cdea
Paul Borm <mail@paulborm.de>
2022-06-03 22:46:24 +0200

    Update site header links and style

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,6 +2,17 @@ module.exports = () => {
   return {
     url: "https://pau.ls",
     title: "Everyday photos — Paul Borm",
+    nav: [
+      {
+        title: "Paul Borm",
+        href: "/",
+        isHome: true,
+      },
+      {
+        title: "photos",
+        href: "/photos/",
+      },
+    ],
     currentDate: () => {
       // You can have a JavaScript function here!
       return new Date().getFullYear();
